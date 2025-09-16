e981553c80a25890c7b6c9e09167b7c24dc84e65
kectx <mateusz.greda@tutore.eu>
2025-09-17 00:47:57 +0200

    feat: initialize frontend with Vite and Vue 3

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueRouter from "unplugin-vue-router/vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(), 
+    VueRouter(
+      {
+        dts: "src/typed-router.d.ts",
+      }
+    ),
+  ],
+  server: {
+    port: 8080,
+  },
+})
