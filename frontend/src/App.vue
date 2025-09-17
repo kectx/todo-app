4a8b80b98a398dd94d96ec28a6c0c8a052cd4b89
Mateusz Greda <xtecek@gmail.com>
2025-09-17 19:34:53 +0200

    feat: add transition on router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <div>
+    <main>
+      <Transition name="fade" mode="out-in">
+        <router-view />
+      </Transition>
+    </main>
+  </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
