62a942de35c511b7dc149a0a321e823a6c1fded4
kectx <mateusz.greda@tutore.eu>
2025-09-17 18:12:43 +0200

    feat: implement backend for todo application with Express and Mongoose

---
+++ backend/routes/todos.js
@@ -0,0 +1,27 @@
+import express from "express";
+import Todo from "../models/Todo.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo.save();
+  res.status(201).json(newTodo);
+});
+
+router.put("/:id", async (req, res) => {
+  const updated = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+router.delete("/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+export default router;
