09b08e4da0c8d723fa47f641a30a4036e9b3b3b0
Mateusz Greda <xtecek@gmail.com>
2025-09-17 22:45:55 +0200

    feat: add Footer component for copyright notice

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <footer class="dark:bg-slate-900">
+    <div class="flex min-h-[60px] w-full items-center justify-center border-t-1">
+      <p class="text-sm">{{ new Date().getFullYear() }} &copy; Todo App. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
