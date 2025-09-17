91e27bcc47065d1bada64cc00f2710a4196d803c
Mateusz Greda <xtecek@gmail.com>
2025-09-17 18:54:53 +0200

    fix: correct DOCTYPE declaration in index.html; update import statements in vite.config.ts for consistency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VueRouter from "unplugin-vue-router/vite"
+import VueRouter from 'unplugin-vue-router/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), 
-    VueRouter(
-      {
-        dts: "src/typed-router.d.ts",
-      }
-    ),
+    VueDevTools(),
+    tailwindcss(),
+    vue(),
+    VueRouter({
+      dts: 'src/typed-router.d.ts',
+    }),
   ],
   server: {
     port: 8080,
