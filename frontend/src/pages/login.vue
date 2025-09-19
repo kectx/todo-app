3d9472d3fb9e271b22d8a63ae7674ef2b8660d6e
Mateusz Greda <xtecek@gmail.com>
2025-09-19 19:24:25 +0200

    style: improve layout and formatting of login page

--- frontend/src/pages/login.vue
+++ frontend/src/pages/login.vue
@@ -13,7 +13,7 @@ const forgotPassword = () => {
 </script>
 <template>
   <main class="flex flex-grow items-center justify-center">
-    <div class="w-full max-w-md space-y-8 rounded-xl bg-white p-8 shadow-2xl dark:bg-primary-dark">
+    <div class="dark:bg-primary-dark w-full max-w-md space-y-8 rounded-xl bg-white p-8 shadow-2xl">
       <div class="text-center">
         <h2 class="text-3xl font-bold text-black/90 dark:text-white/90">Zaloguj się</h2>
         <p class="mt-2 text-sm text-black/60 dark:text-white/60">
@@ -27,7 +27,7 @@ const forgotPassword = () => {
           >
           <input
             autocomplete="email"
-            class="mt-1 block w-full rounded-lg border border-black/10 bg-primary-light px-3 py-2 text-black/90 placeholder-black/40 shadow-sm focus:border-primary focus:ring-primary focus:outline-none sm:text-sm dark:border-white/10 dark:bg-black/20 dark:text-white/90 dark:placeholder-white/40"
+            class="bg-primary-light focus:border-primary focus:ring-primary mt-1 block w-full rounded-lg border border-black/10 px-3 py-2 text-black/90 placeholder-black/40 shadow-sm focus:outline-none sm:text-sm dark:border-white/10 dark:bg-black/20 dark:text-white/90 dark:placeholder-white/40"
             id="email"
             name="email"
             required=""
@@ -40,7 +40,7 @@ const forgotPassword = () => {
           >
           <input
             autocomplete="current-password"
-            class="mt-1 block w-full rounded-lg border border-black/10 bg-primary-light px-3 py-2 text-black/90 placeholder-black/40 shadow-sm focus:border-primary focus:ring-primary focus:outline-none sm:text-sm dark:border-white/10 dark:bg-black/20 dark:text-white/90 dark:placeholder-white/40"
+            class="bg-primary-light focus:border-primary focus:ring-primary mt-1 block w-full rounded-lg border border-black/10 px-3 py-2 text-black/90 placeholder-black/40 shadow-sm focus:outline-none sm:text-sm dark:border-white/10 dark:bg-black/20 dark:text-white/90 dark:placeholder-white/40"
             id="password"
             name="password"
             required=""
@@ -49,24 +49,26 @@ const forgotPassword = () => {
         </div>
         <div class="flex items-center justify-end">
           <div class="text-sm">
-            <p class="font-medium text-primary hover:text-primary/80" @click="forgotPassword">
+            <p class="text-primary hover:text-primary/80 font-medium" @click="forgotPassword">
               Zapomniałem hasła
             </p>
           </div>
         </div>
         <div>
           <button
-            class="focus:ring-offset-background-light dark:focus:ring-offset-background-dark flex w-full justify-center rounded-lg border border-transparent bg-primary px-4 py-3 text-sm font-medium text-white shadow-sm hover:bg-primary/90 focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:outline-none"
+            class="focus:ring-offset-background-light dark:focus:ring-offset-background-dark bg-primary hover:bg-primary/90 focus:ring-primary flex w-full justify-center rounded-lg border border-transparent px-4 py-3 text-sm font-medium text-white shadow-sm focus:ring-2 focus:ring-offset-2 focus:outline-none"
             type="submit"
           >
             Zaloguj się
           </button>
         </div>
       </form>
-      <p class="mt-8 text-center text-sm text-black/60 dark:text-white/60">
+      <div class="mt-8 text-center text-sm text-black/60 dark:text-white/60">
         Nie masz konta?
-        <p class="font-medium text-primary hover:text-primary/80" @click="goToRegister"> Zarejestruj się </p>
-      </p>
+        <p class="text-primary hover:text-primary/80 font-medium" @click="goToRegister">
+          Zarejestruj się
+        </p>
+      </div>
     </div>
   </main>
 </template>
