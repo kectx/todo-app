f7277bd6ea86d079c55bfb401b0236b9b7a283eb
Mateusz Greda <xtecek@gmail.com>
2025-09-23 18:33:06 +0200

    feat: add Firebase initialization and authentication setup

---
+++ backend/firebase.ts
@@ -0,0 +1,9 @@
+import admin from "firebase-admin";
+
+import serviceAccount from "./firebase-service-account.json"; 
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+
+export const auth = admin.auth();
