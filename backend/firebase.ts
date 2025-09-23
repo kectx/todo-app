b085ec9194095175043f219e0a5971f82c8c0738
Mateusz Greda <xtecek@gmail.com>
2025-09-23 19:21:14 +0200

    fix: update import statement for firebase-service-account.json to include type declaration

--- backend/firebase.ts
+++ backend/firebase.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-import serviceAccount from "./firebase-service-account.json"; 
+import serviceAccount from "./firebase-service-account.json" with { type: "json" }; 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
