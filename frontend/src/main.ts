eb44bfecae41af71f1316f16055f85cf9f679f6d
Mateusz Greda <xtecek@gmail.com>
2025-09-23 19:51:15 +0200

    feat: integrate Pinia for state management in the application

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import pinia from './store'
 import './styles/style.css'
 const app = createApp(App)
 
-app.use(router).mount('#app')
+app.use(router)
+app.use(pinia)
+
+app.mount('#app')
