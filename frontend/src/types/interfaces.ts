5c302f1e4ce91fe2b5a91ffdc4f8902887b82e8e
Mateusz Greda <xtecek@gmail.com>
2025-10-06 19:14:00 +0200

    feat: add luxon for date handling and update Todo interfaces

--- frontend/src/types/interfaces.ts
+++ frontend/src/types/interfaces.ts
@@ -1,6 +1,16 @@
+import { DateTime } from 'luxon'
+
 export interface Todo {
   _id: string
   text: string
   done: boolean
   dueDate: string
+}
+
+export interface CalendarDay {
+  date: DateTime
+  dateString: string
+  isToday: boolean
+  isPreviousDay: boolean
+  todos: Todo[]
 }
