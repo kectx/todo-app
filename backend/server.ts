c801b4de148a78873342490be94f8e653c6d4bca
Mateusz Greda <xtecek@gmail.com>
2025-12-13 14:52:59 +0100

    chore: update MongoDB connection string to specify database name for improved clarity

--- backend/server.ts
+++ backend/server.ts
@@ -22,7 +22,7 @@ app.use(sessionMiddleware)
 app.use("/api/todos", todoRoutes);
 app.use("/api/auth", authRoutes);
 
-const uri = `mongodb+srv://${process.env.MONGO_TODO_USER}:${encodeURIComponent(process.env.MONGO_TODO_PASSWORD as string)}@todoapp.gn2y6mx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=todoApp`;
+const uri = `mongodb+srv://${process.env.MONGO_TODO_USER}:${encodeURIComponent(process.env.MONGO_TODO_PASSWORD as string)}@todoapp.gn2y6mx.mongodb.net/${process.env.MONGO_DB}/todo_app?retryWrites=true&w=majority`;
 
 mongoose.connect(uri)
   .then(() => {
